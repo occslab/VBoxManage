e0b70fac3fce18c4c6dcff865bbf4da39624509f
Carlos Barajas <12barajasc@gmail.com>
2015-09-01 17:36:42 -0400

    Added brief command syntax for now

---
+++ FXML.py
@@ -0,0 +1,9 @@
+# This will be used to transfer an admin's xml to all EXISTING home folders. 
+# The Assumption is made that an adminstrator has gone through and connected
+# the master to one 'perfect' VirtualBox.xml file.
+
+# $ python3.4 /AdminHome/.VirtualBox /RootUserFolderPath/
+# User1@CSLab:~$ pwd
+# /RootUserFolderPath/user1
+# -> ls /RootUserFolderPath
+# user1 user2 user3 <-
