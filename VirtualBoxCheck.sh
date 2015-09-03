cab94113cdbb1336aeeeab0bc860cfe656e04869
Carlos Barajas <12barajasc@gmail.com>
2015-09-03 17:20:08 -0400

    Indents -> Spces, Added some to rm commands for future use

--- VirtualBoxCheck.sh
+++ VirtualBoxCheck.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 if [ -d "${HOME}/VirtualBox VMs/windows-7" ]
-	then
-	echo VBoxManage showvminfo "windows-7"
-	# VBoxManage unregistervm "windows-7"
+    then
+    VBoxManage showvminfo "windows-7"
+    # VBoxManage unregistervm "windows-7"
+    # rm -rf "${HOME}/VirtualBox VMs/windows-7"
+    # Begin Building Your VM
+
 else
-	echo '-> Folder DNE'
-	echo $HOME
+    # Check to make sure your vm is built
+    echo 'Checking for Current VM'
+    if [ ! -d "${HOME}/VirtualBox VMs/VMNAMEHERE" ]
+        then
+        # Begin Build Your VM
+    fi
 fi
