d130561f499e79d56b1ddb7bfed0f658283b98fa
Carlos Barajas <12barajasc@gmail.com>
2015-09-04 14:18:56 -0400

    Some changes for 'dynamic' paths

--- VirtualBoxCheck.sh
+++ VirtualBoxCheck.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 oldVMNAME="windows-7"
 newVMNAME="fall2015"
+initpath="./virtualbox-init"
 if [ -d "${HOME}/VirtualBox VMs/${oldVMNAME}" ]
     then
     VBoxManage showvminfo $oldVMNAME
     VBoxManage unregistervm "${oldVMName}" --delete
     # rm -rf "${HOME}/VirtualBox VMs/windows-7"
     # Begin Building Your VM
-
+    initpath
 else
     # Check to make sure your vm is built
     echo 'Checking for Current VM'
     if [ ! -d "${HOME}/VirtualBox VMs/${newVMNAME}" ]
         then
         # Begin Building Your VM
+        initpath
     fi
 fi
