67df4c246df2bbaaf55a83ffea84afdd49837a11
Carlos Barajas <12barajasc@gmail.com>
2015-09-04 14:14:59 -0400

    Correct variable assignment now used

--- VirtualBoxCheck.sh
+++ VirtualBoxCheck.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-$oldVMNAME = "windows-7"
-$newVMNAME = "fall2015"
+oldVMNAME="windows-7"
+newVMNAME="fall2015"
 if [ -d "${HOME}/VirtualBox VMs/${oldVMNAME}" ]
     then
     VBoxManage showvminfo $oldVMNAME
-    # VBoxManage unregistervm $oldVMName --delete
+    VBoxManage unregistervm "${oldVMName}" --delete
     # rm -rf "${HOME}/VirtualBox VMs/windows-7"
     # Begin Building Your VM
 
